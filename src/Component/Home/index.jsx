b6b89775efea769dc9d1cd53cf5f962903dc0759
mraaru <anuragpandey123buy@gmail.com>
2020-10-08 05:28:19 +0530

    today

--- src/Component/Home/index.jsx
+++ src/Component/Home/index.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import Top from "../../Pages/Component/Layout/Top";
 import Menu from '../../Pages/Component/Layout/Menu';
 import Sidebar from '../../Pages/Component/Layout/Sidebar';
-import Footer from '../../Pages/Component/Layout/Footer'
+import Footer from '../../Pages/Component/Layout/Footer';
+import Homexx from '../../Pages/Component/Container/Homex/Homexx'
 export class Home extends Component {
   render() {
     return (
@@ -10,7 +11,8 @@ export class Home extends Component {
             <Top />
             
             <Menu />
-            <br/><br/><br/>
+            <br /><br /><br />
+            <Homexx />
             <Footer />
             <Sidebar />
 
